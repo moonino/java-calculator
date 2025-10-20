bc749ef0cd436803126785486bf0fd193f54df16
moonino <dlsgh4090@gmail.com>
2025-10-20 21:54:53 +0900

    refactor: 테스트 통과를 위해 예외 처리 방식 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -10,14 +10,7 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input= Console.readLine();
 
-        //7. 애플리케이션 흐름 제어
-        try {
-            int result = calculator.add(input);
-            System.out.println("결과: " + result);
-        } catch (IllegalArgumentException e) {
-            // StringCalculator에서 발생한 모든 예외를 여기서 처리합니다.
-            System.out.println("오류: " + e.getMessage());
-        }
-
+        int result = calculator.add(input);
+        System.out.println("결과: " + result);
     }
 }
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -43,4 +43,3 @@ public class StringCalculator {
         return text.split("[,:]");
     }
 }
-
