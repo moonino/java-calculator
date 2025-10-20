51d53071f58bf753cbd4cade8568d57fb10a3c3e
moonino <dlsgh4090@gmail.com>
2025-10-20 20:30:24 +0900

    feat:사용자 입력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,15 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        // 1. 사용자 입력 기능
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input= Console.readLine();
+
+        System.out.println("입력 확인: "+input);
+
     }
 }
