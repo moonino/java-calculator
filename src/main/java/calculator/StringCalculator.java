50f877b8ec945e51d57efa68d158c0d05605f684
moonino <dlsgh4090@gmail.com>
2025-10-20 20:41:33 +0900

    feat: 기본 구분자 처리 기능 및 숫자 변환 및 합산 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -6,7 +6,14 @@ public class StringCalculator {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        
-        return -1; // 임시 반환 값
+        // 3. 기본 구분자 처리 기능
+        String[] numbers = text.split("[,:]");
+
+        // 4. 숫자 변환 및 합산 기능
+        int sum = 0;
+        for (String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
+        return sum; // 임시 반환 값
     }
 }
